5c40bfb220c450e0e03458fc6206a2e93b651306
Felipe Lira <santosfelipeliipe@gmail.com>
2024-05-17 19:24:36 +0000

    criação dos primeiros estilos

---
+++ src/pages/Home.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+import {HomeContainer } from "./pages/Home.styles.js";
+
+export const HomeContainer = styled.div`
+  color: red;
+  display: flex;
+  justify-content: center;
+  background-color: papayawhip;
+`;
